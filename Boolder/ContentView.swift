43608e79119eaf545ab7c46534d810aa794fa692
Nicolas Mondollot <nmondollot@gmail.com>
2020-04-24 15:50:45 +0200

    Initial Commit

---
+++ Boolder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Boolder
+//
+//  Created by Nicolas Mondollot on 24/04/2020.
+//  Copyright © 2020 Nicolas Mondollot. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
