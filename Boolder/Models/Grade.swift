42d6d030d664d6e3f2d6c36f4834b61420cb8901
Nicolas Mondollot <nmondollot@gmail.com>
2020-11-04 17:34:14 +0100

    Use ranges with Grade

--- Boolder/Models/Grade.swift
+++ Boolder/Models/Grade.swift
@@ -6,11 +6,7 @@
 //  Copyright © 2020 Nicolas Mondollot. All rights reserved.
 //
 
-enum GradeError: Error {
-    case incorrectFormat
-}
-
-struct Grade: Comparable, Hashable, CustomStringConvertible {
+struct Grade: Comparable, Hashable, CustomStringConvertible, Strideable {
     let string: String
     
     static let grades = ["1a", "1a+", "1b", "1b+", "1c", "1c+", "2a", "2a+", "2b", "2b+", "2c", "2c+", "3a", "3a+", "3b", "3b+", "3c", "3c+", "4a", "4a+", "4b", "4b+", "4c", "4c+", "5a", "5a+", "5b", "5b+", "5c", "5c+", "6a", "6a+", "6b", "6b+", "6c", "6c+", "7a", "7a+", "7b", "7b+", "7c", "7c+", "8a", "8a+", "8b", "8b+", "8c", "8c+", "9a", "9a+", "9b", "9b+", "9c", "9c+"]
@@ -29,9 +25,15 @@ struct Grade: Comparable, Hashable, CustomStringConvertible {
         }
     }
     
+    init(index: Int) {
+        string = Grade.grades[index % Grade.grades.count]
+    }
+    
     func category() -> Int {
         Int(String(string.first!))!
     }
+    
+    // MARK: CustomStringConvertible protocol
     
     var description: String {
         return string
@@ -50,4 +52,17 @@ struct Grade: Comparable, Hashable, CustomStringConvertible {
     static func == (lhs: Grade, rhs: Grade) -> Bool {
         return lhs.string == rhs.string
     }
+    
+    // MARK: Strideable protocol
+    
+    public func distance(to other: Grade) -> Grade.Stride {
+        return Stride(other.index()) - Stride(self.index())
+    }
+
+    public func advanced(by n: Grade.Stride) -> Grade {
+        let newIndex = Stride(self.index() + n)
+        return Grade(index: newIndex)
+    }
+
+    public typealias Stride = Int
 }
