ac7bb837e133250de2406c4034ef91baae1fb2ac
Nicolas Mondollot <nmondollot@gmail.com>
2021-01-07 15:33:49 +0100

    Move exif logic to file

--- Boolder/UI/Area/Problem details/Photo Capture/ImagePicker.swift
+++ Boolder/UI/Area/Problem details/Photo Capture/ImagePicker.swift
@@ -10,58 +10,6 @@ import UIKit
 import SwiftUI
 import CoreLocation
 
-import Foundation
-import CoreLocation
-import ImageIO
-
-extension CLLocation {
-    
-    func exifMetadata(heading: CLHeading? = nil) -> NSMutableDictionary {
-
-        let GPSMetadata = NSMutableDictionary()
-        let altitudeRef = Int(self.altitude < 0.0 ? 1 : 0)
-        let latitudeRef = self.coordinate.latitude < 0.0 ? "S" : "N"
-        let longitudeRef = self.coordinate.longitude < 0.0 ? "W" : "E"
-
-        // GPS metadata
-        GPSMetadata[(kCGImagePropertyGPSLatitude as String)] = abs(self.coordinate.latitude)
-        GPSMetadata[(kCGImagePropertyGPSLongitude as String)] = abs(self.coordinate.longitude)
-        GPSMetadata[(kCGImagePropertyGPSLatitudeRef as String)] = latitudeRef
-        GPSMetadata[(kCGImagePropertyGPSLongitudeRef as String)] = longitudeRef
-        GPSMetadata[(kCGImagePropertyGPSAltitude as String)] = Int(abs(self.altitude))
-        GPSMetadata[(kCGImagePropertyGPSAltitudeRef as String)] = altitudeRef
-        GPSMetadata[(kCGImagePropertyGPSTimeStamp as String)] = self.timestamp.isoTime()
-        GPSMetadata[(kCGImagePropertyGPSDateStamp as String)] = self.timestamp.isoDate()
-        GPSMetadata[(kCGImagePropertyGPSVersion as String)] = "2.2.0.0"
-        GPSMetadata[(kCGImagePropertyGPSHPositioningError as String)] = horizontalAccuracy
-
-        if let heading = heading {
-            GPSMetadata[(kCGImagePropertyGPSImgDirection as String)] = heading.trueHeading
-            GPSMetadata[(kCGImagePropertyGPSImgDirectionRef as String)] = "T"
-        }
-
-        return GPSMetadata
-    }
-}
-
-extension Date {
-    
-    func isoDate() -> String {
-        let f = DateFormatter()
-        f.timeZone = TimeZone(abbreviation: "UTC")
-        f.dateFormat = "yyyy:MM:dd"
-        return f.string(from: self)
-    }
-    
-    func isoTime() -> String {
-        let f = DateFormatter()
-        f.timeZone = TimeZone(abbreviation: "UTC")
-        f.dateFormat = "HH:mm:ss.SSSSSS"
-        return f.string(from: self)
-    }
-}
-
-
 struct ImagePicker: UIViewControllerRepresentable {
     
     var sourceType: UIImagePickerController.SourceType = .camera
@@ -121,6 +69,8 @@ struct ImagePicker: UIViewControllerRepresentable {
             parent.presentationMode.wrappedValue.dismiss()
         }
         
+        
+        // inspired by https://dev.to/nemecek_f/ios-saving-files-into-user-s-icloud-drive-using-filemanager-4kpm
         func saveImage(_ image: UIImage) {
             // create filename
             let dateFormatter = DateFormatter()
@@ -129,7 +79,14 @@ struct ImagePicker: UIViewControllerRepresentable {
             let dateTime = dateFormatter.string(from: now)
             
             let fileName:String = "your_image_"+dateTime+"_problem_\(parent.problemId)"+".jpg" // name your file the way you want
-            let temporaryFolder:URL = getDocumentsDirectory()
+            var temporaryFolder:URL = getDocumentsDirectory()
+            
+            // FIXME: do it async
+            if let driveURL = FileManager.default.url(forUbiquityContainerIdentifier: nil)?.appendingPathComponent("Documents") {
+                temporaryFolder = driveURL
+            }
+            
+            
             let temporaryFileURL:URL = temporaryFolder.appendingPathComponent(fileName)
 
             // save the image to chosen path
