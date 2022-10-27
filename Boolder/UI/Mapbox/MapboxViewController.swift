bcd259e33892df8db688f7ce2c6fbb77ec01cb54
Nicolas Mondollot <nmondollot@gmail.com>
2022-10-27 17:59:57 +0200

    POC circuit colors

--- Boolder/UI/Mapbox/MapboxViewController.swift
+++ Boolder/UI/Mapbox/MapboxViewController.swift
@@ -44,19 +44,74 @@ class MapboxViewController: UIViewController {
             // Add the vector source to the style
             try! self.mapView.mapboxMap.style.addSource(source, id: sourceIdentifier)
             
+            //            // Define bounding box
+            //            let bounds = CoordinateBounds(southwest: CLLocationCoordinate2D(latitude: 48.2868427, longitude: 2.4806787),
+            //                                          northeast: CLLocationCoordinate2D(latitude: 48.473906, longitude: 2.7698927))
+            //
+            //            // Center the camera on the bounds
+            //            let cameraOptions = mapView.mapboxMap.camera(for: bounds, padding: .zero, bearing: 0, pitch: 0)
+            //            mapView.mapboxMap.setCamera(to: cameraOptions)
+            
+            
+            // Specify a unique string as the layer ID ("LAYER_ID")
+            // and set the source to some source ID (SOURCE_ID).
+            var problemsLayer = CircleLayer(id: "problems")
+            problemsLayer.source = "problems"
+            problemsLayer.sourceLayer = "problems-ayes3a"
+//            problemsLayer.filter =
+            problemsLayer.minZoom = 15
+            
+            let stops: [Double: Double] = [
+              15: 2.0,
+              18: 4.0,
+              22: 16
+            ]
+            
+            // Set some style properties
+            problemsLayer.circleRadius = .expression(
+                Exp(.interpolate) {
+                    ["linear"]
+                    ["zoom"]
+                    stops
+                }
+            )
+            
+            
+            problemsLayer.circleColor = .expression(
+                Exp(.match) {
+                    Exp(.get) { "circuitColor" }
+                    "yellow"
+                    Circuit.CircuitColor.yellow.uicolor
+                    "purple"
+                    Circuit.CircuitColor.purple.uicolor
+                    "orange"
+                    Circuit.CircuitColor.orange.uicolor
+                    "green"
+                    Circuit.CircuitColor.green.uicolor
+                    "blue"
+                    Circuit.CircuitColor.blue.uicolor
+                    "skyblue"
+                    Circuit.CircuitColor.skyBlue.uicolor
+                    "salmon"
+                    Circuit.CircuitColor.salmon.uicolor
+                    "red"
+                    Circuit.CircuitColor.red.uicolor
+                    "black"
+                    Circuit.CircuitColor.black.uicolor
+                    "white"
+                    Circuit.CircuitColor.white.uicolor
+                    Circuit.CircuitColor.offCircuit.uicolor
+                }
+            )
             
             
             
-          // Specify a unique string as the layer ID ("LAYER_ID")
-          // and set the source to some source ID (SOURCE_ID).
-          var circleLayer = CircleLayer(id: "problems")
-          circleLayer.source = "problems"
-            circleLayer.sourceLayer = "problems-ayes3a"
-          // Set some style properties
-          circleLayer.circleRadius = .constant(2)
-          circleLayer.circleColor = .constant(StyleColor(.red))
-          // Add the circle layer to the map.
-            try! self.mapView.mapboxMap.style.addLayer(circleLayer)
+            
+            
+            
+            
+            // Add the circle layer to the map.
+            try! self.mapView.mapboxMap.style.addLayer(problemsLayer)
         }
         
         self.view.addSubview(mapView)
