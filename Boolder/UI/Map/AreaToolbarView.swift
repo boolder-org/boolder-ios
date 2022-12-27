686511eb62f8e670ac2c5847f70b256280bc464d
Nicolas Mondollot <nmondollot@gmail.com>
2022-12-27 15:30:14 +0100

    Filters

--- Boolder/UI/Map/AreaToolbarView.swift
+++ Boolder/UI/Map/AreaToolbarView.swift
@@ -16,126 +16,85 @@ struct AreaToolbarView: View {
     var body: some View {
         VStack {
             HStack {
-                Button {
-                    mapState.selectedArea = nil
-                    mapState.presentProblemDetails = false
-                } label: {
+                HStack {
+                    Button {
+                        mapState.selectedArea = nil
+                        mapState.presentProblemDetails = false
+                    } label: {
+                        Image(systemName: "chevron.left")
+                            .font(Font.body.weight(.semibold))
+                            .foregroundColor(Color(.secondaryLabel))
+                            .padding(.horizontal, 16)
+                        //                        .disabled(true)
+                    }
+                    
+                    Spacer()
+                    
+                    Text(mapState.selectedArea?.name ?? "")
+                        .lineLimit(1)
+                        .truncationMode(.head)
+                    //                    .frame(maxWidth: 400)
+                        .padding(.vertical, 10)
+                    //                    .padding(.horizontal, 25)
+                        .onTapGesture {
+                            mapState.presentProblemDetails = false
+                            presentAreaView = true
+                        }
+                    //                    .background(Color.red)
+                    
+                    Button {
+                        mapState.presentProblemDetails = false
+                        presentAreaView = true
+                    } label: {
+                        Image(systemName: "info.circle")
+                        //                        .background(Color.red)
+                        //                        .foregroundColor(.green)
+                        //                        .padding(.leading, 10)
+                        //                        .disabled(true)
+                    }
+                    
+                    Spacer()
+                    
+                    // quick hack to be able to center the text
                     Image(systemName: "chevron.left")
                         .font(Font.body.weight(.semibold))
                         .foregroundColor(Color(.secondaryLabel))
                         .padding(.horizontal, 16)
-//                        .disabled(true)
+                        .opacity(0)
+                    
                 }
+                .background(Color(.systemBackground))
+                .cornerRadius(12)
+                .shadow(color: Color(.secondaryLabel).opacity(0.5), radius: 5)
                 
-                Spacer()
-                
-                Text(mapState.selectedArea?.name ?? "")
-                    .lineLimit(1)
-                    .truncationMode(.head)
-//                    .frame(maxWidth: 400)
-                    .padding(.vertical, 10)
-//                    .padding(.horizontal, 25)
-                    .onTapGesture {
-                        mapState.presentProblemDetails = false
-                        presentAreaView = true
+                .padding(.top, 8)
+                .sheet(isPresented: $presentAreaView) {
+                    NavigationView {
+                        AreaView(viewModel: AreaViewModel(area: mapState.selectedArea!, mapState: mapState))
                     }
-//                    .background(Color.red)
-                
-                Button {
-                    mapState.presentProblemDetails = false
-                    presentAreaView = true
-                } label: {
-                    Image(systemName: "info.circle")
-//                        .background(Color.red)
-//                        .foregroundColor(.green)
-//                        .padding(.leading, 10)
-//                        .disabled(true)
                 }
-                
-                Spacer()
-                
-                // quick hack to be able to center the text
-                Image(systemName: "chevron.left")
-                    .font(Font.body.weight(.semibold))
-                    .foregroundColor(Color(.secondaryLabel))
-                    .padding(.horizontal, 16)
-                    .opacity(0)
-                
+
             }
-            .background(Color(.systemBackground))
-            .cornerRadius(12)
-            .shadow(color: Color(.secondaryLabel).opacity(0.5), radius: 5)
             .padding(.horizontal)
-            .padding(.top, 8)
-            .sheet(isPresented: $presentAreaView) {
-                NavigationView {
-                    AreaView(viewModel: AreaViewModel(area: mapState.selectedArea!, mapState: mapState))
-                }
-            }
             
-            HStack {
-                
-                Button(action: {
-                    mapState.presentFilters = true
-                }) {
-                    
-                    Label("Niveaux", systemImage: "chevron.down")
-                }
-                .sheet(isPresented: $mapState.presentFilters, onDismiss: {
-                    mapState.filtersRefresh()
-                    // TODO: update $mapState.filters only on dismiss
-                }) {
-                    FiltersView(presentFilters: $mapState.presentFilters, filters: $mapState.filters)
-                        .modify {
-                            if #available(iOS 16, *) {
-                                $0.presentationDetents([.medium]).presentationDragIndicator(.hidden) // TODO: use heights?
-                            }
-                            else {
-                                $0
-                            }
-                        }
-                }
-                
-                // TODO: hide if there is no circuit in the selected area
-                
-                Button(action: {
-                    mapState.presentProblemDetails = false
-                    mapState.presentCircuitPicker = true
-                    
-                }) {
-                    HStack {
-                        Label("Circuits", systemImage: "chevron.down")
-//                        if let circuit = mapState.selectedCircuit, circuitBelongsToArea {
-//                            CircleView(number: "", color: circuit.color.uicolor, height: 16)
-//                        }
-                    }
-                }
-                .sheet(isPresented: $mapState.presentCircuitPicker, onDismiss: {
-                    
-                }) {
-                    CircuitPickerView(viewModel: AreaViewModel(area: mapState.selectedArea!, mapState: mapState))
-                        .modify {
-                            if #available(iOS 16, *) {
-                                $0.presentationDetents([.medium]).presentationDragIndicator(.hidden) // TODO: use heights?
-                            }
-                            else {
-                                $0
-                            }
-                        }
-                }
-                
-//                if mapState.selectedCircuit != nil {
+//            if let circuit = mapState.selectedCircuit {
+//                HStack {
+//                    Button {
+//                        mapState.unselectCircuit()
+//                    } label: {
+//                        Label(circuit.color.shortName, systemImage: "xmark")
+//                    }
+//
 //                    Button {
 //                        mapState.goToNextCircuitProblem()
 //                    } label: {
-//                        Text("suivant")
+//                        Label("Suivant", systemImage: "chevron.right")
 //                    }
+//                    .padding(.horizontal)
+//
 //                }
-                
-                Spacer()
-            }
-            .padding(.horizontal)
-            
+//                .padding(.horizontal)
+//            }
             
             Spacer()
         }
