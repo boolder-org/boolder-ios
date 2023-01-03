2d6a55ff0c30d51cd9b0343aaa80eb465c768098
Nicolas Mondollot <nmondollot@gmail.com>
2023-01-03 11:59:18 +0100

    Center circuit

--- Boolder/UI/Map/MapboxView.swift
+++ Boolder/UI/Map/MapboxView.swift
@@ -50,6 +50,8 @@ struct MapboxView: UIViewControllerRepresentable {
                 
                 let bounds = CoordinateBounds(southwest: CLLocationCoordinate2D(latitude: area.southWestLat, longitude: area.southWestLon),
                                               northeast: CLLocationCoordinate2D(latitude: area.northEastLat, longitude: area.northEastLon))
+
+                
                 let cameraOptions = vc.mapView.mapboxMap.camera(for: bounds, padding: .init(top: 160, left: 20, bottom: 80, right: 20), bearing: 0, pitch: 0)
                 
                 vc.flyinToSomething = true
@@ -107,13 +109,28 @@ struct MapboxView: UIViewControllerRepresentable {
 //            print("coucou")
             
             if let circuit = mapState.selectedCircuit {
-//                vc.setCircuitAsSelected(circuit: circuit)
                 
-                let bounds = CoordinateBounds(southwest: CLLocationCoordinate2D(latitude: circuit.southWestLat, longitude: circuit.southWestLon),
-                                              northeast: CLLocationCoordinate2D(latitude: circuit.northEastLat, longitude: circuit.northEastLon))
-                let cameraOptions = vc.mapView.mapboxMap.camera(for: bounds, padding: .init(top: 160, left: 20, bottom: 80, right: 20), bearing: 0, pitch: 0)
+                let viewport = vc.mapView.mapboxMap.coordinateBounds(for: CameraOptions(cameraState: vc.mapView.cameraState))
+                print(viewport.southwest)
+                print(viewport.northeast)
+
+                let southWest = CLLocationCoordinate2D(
+                    latitude: min(viewport.southwest.latitude, circuit.southWestLat),
+                    longitude: min(viewport.southwest.longitude, circuit.southWestLon)
+                )
+                
+                let northEast = CLLocationCoordinate2D(
+                    latitude: max(viewport.northeast.latitude, circuit.northEastLat),
+                    longitude: max(viewport.northeast.longitude, circuit.northEastLon)
+                )
+                
+                let bounds = CoordinateBounds(southwest: southWest,northeast: northEast)
+                print(bounds.southwest)
+                print(bounds.northeast)
+
+                let cameraOptions = vc.mapView.mapboxMap.camera(for: bounds, padding: .init(top: 0, left: 0, bottom: 0, right: 0), bearing: 0, pitch: 0)
                 vc.flyinToSomething = true
-                vc.mapView.camera.fly(to: cameraOptions, duration: 1) { _ in vc.flyinToSomething = false }
+                vc.mapView.camera.fly(to: cameraOptions, duration: 0.5) { _ in vc.flyinToSomething = false }
             }
             else {
                 vc.unselectCircuit()
