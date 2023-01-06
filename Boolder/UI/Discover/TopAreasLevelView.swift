1e211fd52dcf22654ec363290414db50dbcf3828
Nicolas Mondollot <nmondollot@gmail.com>
2023-01-06 15:05:18 +0100

    UI

--- Boolder/UI/Discover/TopAreasLevelView.swift
+++ Boolder/UI/Discover/TopAreasLevelView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TopAreasLevelView: View {
     @Environment(\.openURL) var openURL
     
-    @State private var level = 5
+    @State private var level = 6
     
     @Binding var appTab: ContentView.Tab
     let mapState: MapState
@@ -25,7 +25,7 @@ struct TopAreasLevelView: View {
                     
                     VStack {
                         Picker("top_areas.level.level", selection: $level) {
-                            Image(systemName: "face.smiling").foregroundColor(.green).tag(3)
+                            Text("1 → 3").tag(3)
                             ForEach(4..<9) { level in
                                 Text(String(level)).tag(level)
                             }
@@ -37,8 +37,11 @@ struct TopAreasLevelView: View {
                     .padding(.vertical)
                     
                     if level == 3 {
-                        VStack {
-                            
+                        VStack(alignment: .leading) {
+//                            Text("Débutant")
+//                                .font(.title2).bold()
+//                                .padding(.horizontal)
+//                                .padding(.bottom)
                             
                             Divider() //.padding(.leading)
                             
@@ -72,6 +75,15 @@ struct TopAreasLevelView: View {
                     }
                     else {
                         VStack {
+//                            VStack(alignment: .leading, spacing: 4) {
+//                                Text("Secteurs avec voies de **niveau \(String(level))**")
+////                                    .foregroundColor(.gray)
+//                                    .frame(maxWidth: .infinity, alignment: .leading)
+//                            }
+//
+//                                .padding(.horizontal)
+//                                .padding(.bottom)
+                            
                             Divider() //.padding(.leading)
                             
                             ForEach(Area.allWithLevel(level).filter{$0.problemsCount >= 5}.prefix(20)) { areaWithCount in
@@ -112,6 +124,7 @@ struct TopAreasLevelView: View {
                     }
                 }
 //                .padding(.horizontal)
+                .padding(.bottom)
             }
         }
         .navigationTitle("top_areas.level.title")
