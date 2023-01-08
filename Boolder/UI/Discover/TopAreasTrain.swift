47f65c3cccf8155548fe7f901147c92ec0f6cfea
Nicolas Mondollot <nmondollot@gmail.com>
2023-01-08 10:07:39 +0100

    Add new train stations

--- Boolder/UI/Discover/TopAreasTrain.swift
+++ Boolder/UI/Discover/TopAreasTrain.swift
@@ -17,14 +17,14 @@ struct TopAreasTrain: View {
     var body: some View {
         GeometryReader { geo in
             ScrollView {
-                VStack(alignment: .leading, spacing: 16) {
+                VStack(alignment: .leading, spacing: 24) {
                     
-                    VStack(alignment: .leading, spacing: 24) {
+                    VStack(alignment: .leading, spacing: 16) {
                         
-                        VStack(alignment: .leading) {
-                            Text("Gare de Bois le Roi")
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("Gare de Bois-le-Roi")
                                 .font(.title2).bold()
-                                .padding(.bottom, 4)
+//                                .padding(.bottom, 2)
                             
                             Text("Secteurs à moins de 20 min en vélo :")
                                 .font(.body)
@@ -64,7 +64,7 @@ struct TopAreasTrain: View {
                                 Button(action: {
                                     openURL(URL(string: "https://www.horaires-de-trains.fr/horaires-Paris_Gare_de_Lyon-Bois_le_Roi.html")!)
                                 }) {
-                                    Text("Horaires")
+                                    Text("Horaires depuis Paris Gare de Lyon")
                                         .foregroundColor(Color.appGreen)
                                 }
                             }
@@ -75,12 +75,12 @@ struct TopAreasTrain: View {
                     .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
                     .padding(.vertical, 8)
                     
-                    VStack(alignment: .leading, spacing: 24) {
+                    VStack(alignment: .leading, spacing: 16) {
                         
-                        VStack(alignment: .leading) {
+                        VStack(alignment: .leading, spacing: 4) {
                             Text("Gare Fontainebleau Avon")
                                 .font(.title2).bold()
-                                .padding(.bottom, 4)
+//                                .padding(.bottom, 2)
                             
                             Text("Secteurs à moins de 20 min en vélo :")
                                 .font(.body)
@@ -120,7 +120,175 @@ struct TopAreasTrain: View {
                                 Button(action: {
                                     openURL(URL(string: "https://www.horaires-de-trains.fr/horaires-Paris_Gare_de_Lyon-Fontainebleau_Avon.html")!)
                                 }) {
-                                    Text("Horaires")
+                                    Text("Horaires depuis Paris Gare de Lyon")
+                                        .foregroundColor(Color.appGreen)
+                                }
+                            }
+//                            .padding(.vertical)
+                        }
+                        .padding(.horizontal)
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                    .padding(.vertical, 8)
+                    
+                    VStack(alignment: .leading, spacing: 16) {
+                        
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("Gare Montigny-sur-Loing")
+                                .font(.title2).bold()
+//                                .padding(.bottom, 2)
+                            
+                            Text("Secteurs à moins de 20 min en vélo :")
+                                .font(.body)
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.horizontal)
+                        
+                        VStack(alignment: .leading) {
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(alignment: .top, spacing: 0) {
+                                    
+                                    Color.white.opacity(0)
+                                        .frame(width: 0, height: 1)
+                                        .padding(.leading, 8)
+                                    
+                                    ForEach(areasFromMontigny) { area in
+                                        NavigationLink {
+                                            AreaView(area: area, mapState: mapState, appTab: $appTab, linkToMap: true)
+                                        } label: {
+                                            AreaCardView(area: area, width: abs(geo.size.width-16*2-8)/2, height: abs(geo.size.width-16*2-8)/2*9/16)
+                                                .padding(.leading, 8)
+                                                .contentShape(Rectangle())
+                                        }
+                                    }
+                                    
+                                    Color.white.opacity(0)
+                                        .frame(width: 0, height: 1)
+                                        .padding(.trailing, 16)
+                                }
+                            }
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            
+                            HStack(alignment: .top, spacing: 4) {
+                                Button(action: {
+                                    openURL(URL(string: "https://www.horaires-de-trains.fr/horaires-Paris_Gare_de_Lyon-Montigny_Sur_Loing.html")!)
+                                }) {
+                                    Text("Horaires depuis Paris Gare de Lyon")
+                                        .foregroundColor(Color.appGreen)
+                                }
+                            }
+//                            .padding(.vertical)
+                        }
+                        .padding(.horizontal)
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                    .padding(.vertical, 8)
+                    
+                    VStack(alignment: .leading, spacing: 16) {
+                        
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("Gare de Nemours Saint Pierre")
+                                .font(.title2).bold()
+//                                .padding(.bottom, 2)
+                            
+                            Text("Secteurs à moins de 10 min en vélo :")
+                                .font(.body)
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.horizontal)
+                        
+                        VStack(alignment: .leading) {
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(alignment: .top, spacing: 0) {
+                                    
+                                    Color.white.opacity(0)
+                                        .frame(width: 0, height: 1)
+                                        .padding(.leading, 8)
+                                    
+                                    ForEach(areasFromNemours) { area in
+                                        NavigationLink {
+                                            AreaView(area: area, mapState: mapState, appTab: $appTab, linkToMap: true)
+                                        } label: {
+                                            AreaCardView(area: area, width: abs(geo.size.width-16*2-8)/2, height: abs(geo.size.width-16*2-8)/2*9/16)
+                                                .padding(.leading, 8)
+                                                .contentShape(Rectangle())
+                                        }
+                                    }
+                                    
+                                    Color.white.opacity(0)
+                                        .frame(width: 0, height: 1)
+                                        .padding(.trailing, 16)
+                                }
+                            }
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            
+                            HStack(alignment: .top, spacing: 4) {
+                                Button(action: {
+                                    openURL(URL(string: "https://www.horaires-de-trains.fr/horaires-Paris_Gare_de_Lyon-Nemours_Saint_Pierre.html")!)
+                                }) {
+                                    Text("Horaires depuis Paris Gare de Lyon")
+                                        .foregroundColor(Color.appGreen)
+                                }
+                            }
+//                            .padding(.vertical)
+                        }
+                        .padding(.horizontal)
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                    .padding(.vertical, 8)
+                    
+                    VStack(alignment: .leading, spacing: 16) {
+                        
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("Gare de Malesherbes")
+                                .font(.title2).bold()
+//                                .padding(.bottom, 2)
+                            
+                            Text("Secteurs à moins de 20 min en vélo :")
+                                .font(.body)
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.horizontal)
+                        
+                        VStack(alignment: .leading) {
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(alignment: .top, spacing: 0) {
+                                    
+                                    Color.white.opacity(0)
+                                        .frame(width: 0, height: 1)
+                                        .padding(.leading, 8)
+                                    
+                                    ForEach(areasFromMalesherbes) { area in
+                                        NavigationLink {
+                                            AreaView(area: area, mapState: mapState, appTab: $appTab, linkToMap: true)
+                                        } label: {
+                                            AreaCardView(area: area, width: abs(geo.size.width-16*2-8)/2, height: abs(geo.size.width-16*2-8)/2*9/16)
+                                                .padding(.leading, 8)
+                                                .contentShape(Rectangle())
+                                        }
+                                    }
+                                    
+                                    Color.white.opacity(0)
+                                        .frame(width: 0, height: 1)
+                                        .padding(.trailing, 16)
+                                }
+                            }
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            
+                            HStack(alignment: .top, spacing: 4) {
+                                Button(action: {
+                                    openURL(URL(string: "https://www.horaires-de-trains.fr/horaires-Paris_Gare_de_Lyon-Malesherbes.html")!)
+                                }) {
+                                    Text("Horaires depuis Paris Gare de Lyon")
                                         .foregroundColor(Color.appGreen)
                                 }
                             }
@@ -131,8 +299,7 @@ struct TopAreasTrain: View {
                     .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
                     .padding(.vertical, 8)
                 }
-//                .padding(.horizontal)
-                .padding(.top)
+                .padding(.vertical)
             }
         }
         .navigationTitle("top_areas.train.title")
@@ -146,6 +313,18 @@ struct TopAreasTrain: View {
     
     var areasFromAvon: [Area] {
         [50,53,33,52].map{Area.load(id: $0)}.compactMap{$0}
+    }
+    
+    var areasFromMontigny: [Area] {
+        [51,68].map{Area.load(id: $0)}.compactMap{$0}
+    }
+    
+    var areasFromNemours: [Area] {
+        [32].map{Area.load(id: $0)}.compactMap{$0}
+    }
+    
+    var areasFromMalesherbes: [Area] {
+        [23].map{Area.load(id: $0)}.compactMap{$0}
     }
 }
 
