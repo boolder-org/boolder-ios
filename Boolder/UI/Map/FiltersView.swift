533ef4bd380afadbd9600c0caf5eb83bf4ce21ba
Nicolas Mondollot <nmondollot@gmail.com>
2023-01-11 14:26:35 +0100

    Refactor

--- Boolder/UI/Map/FiltersView.swift
+++ Boolder/UI/Map/FiltersView.swift
@@ -15,87 +15,69 @@ struct FiltersView: View {
     
     let mapState: MapState
     
-    @State private var segment: Segment = .circuit
-    
-    enum Segment {
-        case circuit
-        case level
-    }
-    
     var body: some View {
         NavigationView {
-
-                levels
-                .navigationBarTitle("filters.levels", displayMode: .inline)
-                .navigationBarItems(
-                    leading: Button(action: {
-                        mapState.clearFilters()
-                        mapState.unselectCircuit()
-                        presentationMode.wrappedValue.dismiss()
-                    }) {
-                        Text("filters.clear")
-                            .padding(.vertical)
-                            .font(.body)
-                    },
-                    trailing: Button(action: {
-                        presentationMode.wrappedValue.dismiss()
-                    }) {
-                        Text("OK")
-                            .bold()
-                            .padding(.vertical)
-                            .padding(.leading, 32)
-                    }
-                )
-//            }
-        }
-//        .navigationViewStyle(StackNavigationViewStyle())
-    }
-
-    
-    var levels: some View {
-        List {
-            Section {
-                ForEach([GradeRange.beginner, GradeRange.level4, GradeRange.level5, GradeRange.level6, GradeRange.level7], id: \.self) { range in
-                    Button(action: {
-                        if filters.gradeRange == range {
-                            filters.gradeRange = nil
-                        }
-                        else {
-                            filters.gradeRange = range
-                            presentationMode.wrappedValue.dismiss()
-                        }
-                    }) {
-                        HStack {
-                            Image(systemName: filters.gradeRange == range ? "largecircle.fill.circle" : "circle")
-                                .font(Font.body.weight(.bold)).frame(width: 20, height: 20)
-                            
-                            Text(range.description).foregroundColor(.primary)
-                            Spacer()
-                            if(range == .beginner) {
-                                Text("filters.beginner").foregroundColor(Color(.systemGray))
+            
+            List {
+                Section {
+                    ForEach([GradeRange.beginner, GradeRange.level4, GradeRange.level5, GradeRange.level6, GradeRange.level7], id: \.self) { range in
+                        Button {
+                            if filters.gradeRange == range {
+                                filters.gradeRange = nil
+                            }
+                            else {
+                                filters.gradeRange = range
+                                presentationMode.wrappedValue.dismiss()
+                            }
+                        } label : {
+                            HStack {
+                                Image(systemName: filters.gradeRange == range ? "largecircle.fill.circle" : "circle")
+                                    .font(Font.body.weight(.bold)).frame(width: 20, height: 20)
+                                
+                                Text(range.description).foregroundColor(.primary)
+                                Spacer()
+                                if(range == .beginner) {
+                                    Text("filters.beginner").foregroundColor(Color(.systemGray))
+                                }
                             }
                         }
                     }
                 }
-            }
-            
-            Section {
                 
-                NavigationLink(destination:
-                                GradeRangePickerView(gradeRange: filters.gradeRange ?? GradeRange(min: Grade("1a"), max: Grade("9a+")), onSave: { range in
-                    filters.gradeRange = range
-                })
-                ) {
-                    HStack {
-                        Image(systemName: (filters.gradeRange?.isCustom ?? false) ? "largecircle.fill.circle" : "circle")
-                            .font(Font.body.weight(.bold)).frame(width: 20, height: 20).foregroundColor(.appGreen)
-                        
-                        Text("filters.grade.range.custom").foregroundColor(.primary)
-                        Spacer()
-                        Text(customRangeDescription).foregroundColor(Color(.systemGray)).font(.caption)
+                Section {
+                    
+                    NavigationLink(destination: GradeRangePickerView(gradeRange: filters.gradeRange ?? GradeRange(min: Grade("1a"), max: Grade("9a+")), onSave: { range in filters.gradeRange = range})) {
+                        HStack {
+                            Image(systemName: (filters.gradeRange?.isCustom ?? false) ? "largecircle.fill.circle" : "circle")
+                                .font(Font.body.weight(.bold)).frame(width: 20, height: 20).foregroundColor(.appGreen)
+                            
+                            Text("filters.grade.range.custom").foregroundColor(.primary)
+                            Spacer()
+                            Text(customRangeDescription).foregroundColor(Color(.systemGray)).font(.caption)
+                        }
                     }
                 }
             }
+            .navigationBarTitle("filters.levels", displayMode: .inline)
+            .navigationBarItems(
+                leading: Button {
+                    mapState.clearFilters()
+                    mapState.unselectCircuit()
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("filters.clear")
+                        .padding(.vertical)
+                        .font(.body)
+                },
+                trailing: Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("OK")
+                        .bold()
+                        .padding(.vertical)
+                        .padding(.leading, 32)
+                }
+            )
         }
     }
     
