b35c02898ce70eb9bb7edb7402e5ff76bc67a6c7
Nicolas Mondollot <nmondollot@gmail.com>
2023-05-05 19:01:28 +0200

    Auto create favorites

--- Boolder/UI/Map/MapContainerView.swift
+++ Boolder/UI/Map/MapContainerView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 import CoreLocation
+import CoreData
 
 struct MapContainerView: View {
     @EnvironmentObject var odrManager: ODRManager
@@ -34,7 +35,14 @@ struct MapContainerView: View {
                 .opacity(mapState.selectedArea != nil ? 1 : 0)
         }
         .onAppear {
+            loadTop7a()
             DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [self] in
+                mapState.filtersRefreshCount += 1
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) { [self] in
+                mapState.filtersRefreshCount += 1
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 30.0) { [self] in
                 mapState.filtersRefreshCount += 1
             }
         }
@@ -57,6 +65,44 @@ struct MapContainerView: View {
                 mapState.displayCircuitStartButton = true
                 mapState.presentAreaView = false
             }
+        }
+    }
+    
+    func loadTop7a() {
+        print("load 7a")
+        if favorites().isEmpty {
+            let ids = [15552,15555,15553,15658,15625,15560,224,230,10873,10869,245,243,237,9074,14558,14199,14179,14136,14343,1739,1703,10045,10042,1088,915,914,881,1032,5397,5326,5325,1856,1840,1861,1816,1913,2021,2003,2094,2087,11310,2668,2857,2542,2545,2543,2527,8680,8801,7807,2542,1582,1462,1455,1579,1526,1435,13478,13484,13468,13467,13074,12998,13070,13011,13042,13040,13030,2893,2911,2902,2889,3091,3092,3161,3186,11425,6342,6344,3257,3255,3283,3288,3331,3409,4534,4551,4581,690,5091,5106,5124,5088,5127,7878,3825,3803,4787,9237,9177,9302,9345,2315,2214,7756,2352,8131,8126]
+            
+            ids.forEach { id in
+                let favorite = Favorite(context: managedObjectContext)
+                favorite.id = UUID()
+                favorite.problemId = Int64(id)
+                favorite.createdAt = Date()
+                
+                do {
+                    try managedObjectContext.save()
+                } catch {
+                    // handle the Core Data error
+                }
+            }
+            
+            print("done")
+        }
+        else {
+            print("nothing to load")
+        }
+    }
+    
+    func favorites() -> [Favorite] {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let request: NSFetchRequest<Favorite> = Favorite.fetchRequest()
+        request.sortDescriptors = []
+        
+        do {
+            return try context.fetch(request)
+        } catch {
+            fatalError("Failed to fetch favorites: \(error)")
         }
     }
     
