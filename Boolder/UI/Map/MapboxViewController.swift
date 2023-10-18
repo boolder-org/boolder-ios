d3d032dec420267e69df5352bf67a875c1bfd1ea
Mirko Romstadt <mirko.romstadt@innovation-center.com>
2023-10-18 10:17:59 +0200

    Don't zoom out when tapping on locate button

--- Boolder/UI/Map/MapboxViewController.swift
+++ Boolder/UI/Map/MapboxViewController.swift
@@ -737,7 +737,7 @@ class MapboxViewController: UIViewController {
                 let cameraOptions = CameraOptions(
                     center: location.coordinate,
                     padding: safePadding,
-                    zoom: 17
+                    zoom: mapView.cameraState.zoom > 17 ? mapView.cameraState.zoom : 17
                 )
                 
                 flyTo(cameraOptions)
