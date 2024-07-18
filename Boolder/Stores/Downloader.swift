30fea557828ac821121818e64fdfd7db06cd5cdc
Nicolas Mondollot <nmondollot@gmail.com>
2024-07-18 11:35:43 +0200

    Use download task instead of data task, remove retry

--- Boolder/Stores/Downloader.swift
+++ Boolder/Stores/Downloader.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class Downloader : ObservableObject {
     @Published var progress: Double = 0
-    private let maxRetries: Int
+    private let maxRetries: Int // TODO: remove
     private let topos: [TopoData]
     private var count: Int = 0
     
@@ -41,7 +41,7 @@ class Downloader : ObservableObject {
                         return true
                     }
                     else {
-                        return await self.downloadFile(topo: topo, retriesLeft: self.maxRetries)
+                        return await self.downloadFile(topo: topo)
                     }
                     
                 }
@@ -87,61 +87,39 @@ class Downloader : ObservableObject {
         FileManager.default.fileExists(atPath: topo.fileUrl.path)
     }
     
-    private func downloadFile(topo: TopoData, retriesLeft: Int) async -> Bool {
+    private func downloadFile(topo: TopoData) async -> Bool {
         print("downloading topo \(topo.id)")
         
-//        try? await Task.sleep(nanoseconds: 1_000_000_000*UInt64(Int.random(in: 0...5))) // FIXME: remove
+        //        try? await Task.sleep(nanoseconds: 1_000_000_000*UInt64(Int.random(in: 0...5))) // FIXME: remove
         
         let config = URLSessionConfiguration.default
-        config.timeoutIntervalForRequest = 30
-//        config.waitsForConnectivity = false
-        let session = URLSession(configuration: config) 
+        config.timeoutIntervalForRequest = 60
+        //        config.waitsForConnectivity = false
+        let session = URLSession(configuration: config)
         
-        do {
-        
-            
-            let (data, response) = try await session.data(from: topo.url)
-            
-//            if Int.random(in: 0...30) == 0 {
-//                print("BUGG for topo \(topo.id)")
-//                try? await Task.sleep(nanoseconds: 3_000_000_000) // FIXME: remove
-//                return false
-//            }
-            
-            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
-                save(data: data, for: topo)
-                count += 1
-                progress = min(1.0, Double(count) / Double(topos.count))
-                return true
-//                print("Downloaded and saved \(topo.url)")
-            } else if retriesLeft > 0 {
-                print("Retrying \(topo.url), retries left: \(retriesLeft)")
-                try? await Task.sleep(nanoseconds: 1_000_000_000)
-                return await downloadFile(topo: topo, retriesLeft: retriesLeft - 1)
-            } else {
-                print("Failed to download \(topo.url) after maximum retries")
-            }
-        }
-        catch {
-            // TODO: exception or cancelation
-            print(error)
-//            print(error.localizedDescription)
+        if let (localURL, _) = try? await session.download(from: topo.url) {
+            save(localURL: localURL, for: topo)
+            count += 1
+            progress = min(1.0, Double(count) / Double(topos.count))
+            return true
         }
         
         return false
+        
+        
     }
     
-    private func save(data: Data, for topo: TopoData) {
+    private func save(localURL: URL, for topo: TopoData) {
         let fileManager = FileManager.default
         let documentsURL = fileManager.urls(for: .cachesDirectory, in: .userDomainMask)[0]
-        let fileURL = documentsURL.appendingPathComponent("area-\(topo.areaId)").appendingPathComponent("topo-\(topo.id).jpg")
+        let destinationURL = documentsURL.appendingPathComponent("area-\(topo.areaId)").appendingPathComponent("topo-\(topo.id).jpg")
         
-        do {
-            try data.write(to: fileURL)
-//            print("File saved: \(fileURL)")
-        } catch {
-            print("Failed to save file \(topo.url): \(error)")
+        if fileManager.fileExists(atPath: destinationURL.path) {
+            try? fileManager.removeItem(at: destinationURL)
         }
+        
+        // Move the downloaded file to the destination URL
+        try? fileManager.moveItem(at: localURL, to: destinationURL)
     }
     
     func createFolderInCachesDirectory(folderName: String) {
