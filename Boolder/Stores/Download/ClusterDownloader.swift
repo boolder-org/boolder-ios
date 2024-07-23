9e394710ee5abb36096f10c0d541bdda2fbcb6cd
Nicolas Mondollot <nmondollot@gmail.com>
2024-07-23 15:29:38 +0200

    Fix stop downloads

--- Boolder/Stores/Download/ClusterDownloader.swift
+++ Boolder/Stores/Download/ClusterDownloader.swift
@@ -13,7 +13,6 @@ class ClusterDownloader: ObservableObject {
     private let cluster: Cluster
     @Published var areas = [AreaDownloader]()
     
-//    private var cancellables2 = Set<AnyCancellable>() // TODO: rename
     private var downloadQueue = [AreaDownloader]()
     private var currentIndex = 0
     @Published var currentDownloader: AreaDownloader?
@@ -52,16 +51,6 @@ class ClusterDownloader: ObservableObject {
         let downloader = downloadQueue[currentIndex]
         currentDownloader = downloader
         
-//        downloader.$status
-//            .filter { $0 == .downloaded }
-//            .sink { [weak self] _ in
-//                self?.currentIndex += 1
-//                self?.startNextDownload()
-//            }
-//            .store(in: &cancellables2)
-        
-        // TODO: handle failure
-        
         downloader.start(onSuccess: { [self] in
             self.currentIndex += 1
             self.startNextDownload()
@@ -72,9 +61,10 @@ class ClusterDownloader: ObservableObject {
     }
     
     func stopDownloads() {
-        areas.filter{ $0.status != .initial }.forEach{ $0.cancel() }
+        areas.filter{ $0.status.downloadingOrQueued }.forEach{ $0.cancel() }
+        
+//        currentDownloader?.cancel()
         downloadQueue = []
-//        cancellables2 = []
     }
     
     var downloading: Bool {
