282cbd00db0ddbe0187759ff5bf71f94eec03327
Nicolas Mondollot <nmondollot@gmail.com>
2024-09-16 10:34:13 +0200

    Revert "Try something"
    This reverts commit 8b6c29438296f6dba2f3aedbc61a0791358b8b41.

--- Boolder/Models/TopoWithPosition.swift
+++ Boolder/Models/TopoWithPosition.swift
@@ -10,16 +10,6 @@ import Foundation
 import UIKit
 import SQLite
 
-struct PaginableTopoWithPosition: Hashable, Identifiable {
-    var id: [Int] {
-//        "\(topo.id)-\(index)"
-        [topo.id, index]
-    }
-    
-    let topo: TopoWithPosition
-    let index: Int
-}
-
 struct TopoWithPosition: Hashable, Identifiable {
     let id: Int
     let boulderId: Int?
--- Boolder/UI/Map/Problem details/Topo/BoulderView.swift
+++ Boolder/UI/Map/Problem details/Topo/BoulderView.swift
@@ -15,20 +15,11 @@ struct BoulderView: View {
     
     @ObservedObject var mapState: MapState
     
-    @State private var scrollTarget: [Int]? = nil
+    @State private var scrollTarget: Int? = nil
     @State private var visibleTopoId: Int? = nil
     
     var topos: [TopoWithPosition] {
         TopoWithPosition.onBoulder(boulderId)
-    }
-    
-    var paginableTopos: [PaginableTopoWithPosition] {
-        var array = [PaginableTopoWithPosition]()
-        array.append(PaginableTopoWithPosition(topo: topos.last!, index: -1))
-        array.append(contentsOf: topos.map{PaginableTopoWithPosition(topo: $0, index: 0)})
-        array.append(PaginableTopoWithPosition(topo: topos.first!, index: 1))
-        return array
-        
     }
     
     var scrollView: some View {
@@ -36,7 +27,7 @@ struct BoulderView: View {
             return ScrollViewReader { proxy in
                 ScrollView(.horizontal, showsIndicators: false) {
                     LazyHStack {
-                        ForEach(paginableTopos) { topo in
+                        ForEach(topos) { topo in
                             ImprovedTopoView(topo: topo, problem: $problem, mapState: mapState)
                                 .tag(topo.id)
                         }
@@ -48,73 +39,41 @@ struct BoulderView: View {
                 .onChange(of: scrollTarget) { target in
                     if let target = target {
 //                        withAnimation {
-                        print("scroll to \(target)")
                             proxy.scrollTo(target, anchor: .center)
 //                        }
                     }
                 }
                 .onChange(of: problem) { newProblem in
-//                    scrollTarget = newProblem.topoId
+                    scrollTarget = newProblem.topoId
                 }
                 .modify {
                     if #available(iOS 18.0, *) {
                         $0.onScrollPhaseChange { oldPhase, newPhase in
                             if newPhase == .idle {
                                 print("changed page")
-//                                print(visibleTopoId)
-                                
-//                                if let visibleTopoId = visibleTopoId {
-//                                    if visibleTopoId == topos.first?.id {
-//                                        print("go to last")
-//                                        scrollTarget = topos.last?.id
-//                                    }
-//                                    else if visibleTopoId == topos.last?.id {
-//                                        print("go to first")
-//                                        scrollTarget = topos.first?.id
-//                                    }
-//                                }
-                                
 //                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                                     if let visibleTopoId = visibleTopoId {
                                         if let t = TopoWithPosition.load(id: visibleTopoId) {
                                             if let first = t.firstProblemOnTheLeft {
-//                                                mapState.selectProblem(first)
+                                                mapState.selectProblem(first)
                                             }
                                         }
                                     }
 //                                }
                             }
                         }
-                        .onScrollTargetVisibilityChange(idType: PaginableTopoWithPosition.ID.self, threshold: 0.5) { array in
-//                            print(array)
-                            if let visibleTopo = array.first {
-//                                print(visibleTopo)
-                                
-                                if visibleTopo[1] == -1 {
-                                    print("left")
-                                    scrollTarget = [topos.last!.id, 0]
-                                }
-                                else if visibleTopo[1] == 1 {
-                                    print("right")
-                                    scrollTarget = [topos.first!.id, 0]
-                                }
-                                
-//                                if first[1] == 0 {
-//                                    visibleTopoId = first[0]
-//                                }
+                        .onScrollTargetVisibilityChange(idType: TopoWithPosition.ID.self, threshold: 0.5) { array in
+                            if let first = array.first {
+                                visibleTopoId = first
 //                                paginateToProblemWithScrollView(p: problem)
                             }
-                            
-//                            print("visible = \(visibleTopoId)")
                         }
                     } else {
                         $0
                     }
                 }
                 .task {
-                    if let topoId = problem.topoId {
-                        scrollTarget = [topoId, 0]
-                    }
+                    scrollTarget = problem.topoId
                 }
             }
         }
@@ -123,38 +82,38 @@ struct BoulderView: View {
         }
     }
     
-//    var tabView: some View {
-//        TabView(selection: $currentPage) {
-//            ForEach(topos) { topo in
-//                ZStack {
-//                    ImprovedTopoView(topo: topo, problem: $problem, mapState: mapState)
-////                        Text(problem.localizedName)
+    var tabView: some View {
+        TabView(selection: $currentPage) {
+            ForEach(topos) { topo in
+                ZStack {
+                    ImprovedTopoView(topo: topo, problem: $problem, mapState: mapState)
+//                        Text(problem.localizedName)
+                }
+                .tag(topo.id)
+            }
+            
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+//            .onChange(of: currentPage) { newPage in
+//                print("Page turned to: \(newPage)")
+//
+//                let topoId = newPage
+//                let topo = TopoWithPosition.load(id: topoId)
+//
+//                // TODO: choose problem on the left
+//                if let first = topo?.problems.first {
+//                    problem = first
 //                }
-//                .tag(topo.id)
 //            }
-//            
-//        }
-//        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-////            .onChange(of: currentPage) { newPage in
-////                print("Page turned to: \(newPage)")
-////
-////                let topoId = newPage
-////                let topo = TopoWithPosition.load(id: topoId)
-////
-////                // TODO: choose problem on the left
-////                if let first = topo?.problems.first {
-////                    problem = first
-////                }
-////            }
-//        .onChange(of: problem) { newProblem in
-//            paginateToProblem(p: newProblem)
-//        }
-//        .onAppear {
-//            if let topoId = problem.topoId {
-//                currentPage = topoId
-//            }
-//        }
-//    }
+        .onChange(of: problem) { newProblem in
+            paginateToProblem(p: newProblem)
+        }
+        .onAppear {
+            if let topoId = problem.topoId {
+                currentPage = topoId
+            }
+        }
+    }
     
     var body: some View {
         ZStack(alignment: .center) {
@@ -170,9 +129,9 @@ struct BoulderView: View {
     func paginateToProblemWithScrollView(p: Problem) {
         print("visible : \(visibleTopoId)")
         print("selected : \(problem.topoId)")
-//        if visibleTopoId != problem.topoId {
-//            scrollTarget = problem.topoId
-//        }
+        if visibleTopoId != problem.topoId {
+            scrollTarget = problem.topoId
+        }
     }
     
     func paginateToProblem(p: Problem) {
