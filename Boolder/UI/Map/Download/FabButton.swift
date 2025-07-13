0801bd392e15874f52fb6d0ed8a5140c0be43da7
Nicolas Mondollot <nmondollot@gmail.com>
2025-07-13 18:36:27 +0200

    Glass effect for fab button

--- Boolder/UI/Map/Download/FabButton.swift
+++ Boolder/UI/Map/Download/FabButton.swift
@@ -12,14 +12,15 @@ struct FabButton: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         return configuration.label
 //            .accentColor(.primary)
-            .opacity(configuration.isPressed ? 0.2 : 1)
-            .frame(width: 18, height: 18)
+//            .opacity(configuration.isPressed ? 0.2 : 1)
+//            .frame(width: 18, height: 18)
             .padding(12)
-            .background(Color.systemBackground)
-            .clipShape(Circle())
-            .overlay(
-                Circle().stroke(Color(.secondaryLabel), lineWidth: 0.25)
-            )
-            .shadow(color: Color(UIColor.init(white: 0.8, alpha: 0.8)), radius: 8)
+//            .background(Color.systemBackground)
+//            .clipShape(Circle())
+//            .overlay(
+//                Circle().stroke(Color(.secondaryLabel), lineWidth: 0.25)
+//            )
+//            .shadow(color: Color(UIColor.init(white: 0.8, alpha: 0.8)), radius: 8)
+            .glassEffect(.regular)
     }
 }
