f5be31b06e9a8d6a89cfddda02b4163433133964
Nicolas Mondollot <nmondollot@gmail.com>
2025-08-04 10:47:43 +0200

    "No image" should take the whole view

--- Boolder/UI/Map/Problem details/Topo/TopoView.swift
+++ Boolder/UI/Map/Problem details/Topo/TopoView.swift
@@ -71,6 +71,7 @@ struct TopoView: View {
                 Image("nophoto")
                     .font(.system(size: 60))
                     .foregroundColor(Color.gray)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
             else if photoStatus == .noInternet || photoStatus == .timeout || photoStatus == .error {
                 VStack(spacing: 16) {
